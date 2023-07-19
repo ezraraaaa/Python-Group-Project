d18f5fe8b5011c7f1be9e61c1886df060c5f83d5
NEXJESsofea <s10255783@connect.np.edu.sg>
2023-07-19 20:59:15 +0800

    trial n error

--- Cash-On-Hand.py
+++ Cash-On-Hand.py
@@ -1,1 +1,32 @@
-a hellooooooo
+## import required modules
+from pathlib import Path
+import csv
+
+# ## we can use this to inspect/check what is cwd:
+# print(Path.cwd())
+
+# ## setup filepaths for reading:
+fp_read = Path.cwd()/"CSV_Reports"/"Cash_on_Hand.csv"
+
+def coh_function():
+    fp_read = Path.cwd()/"CSV_Reports"/"Cash_on_Hand.csv"
+    fp_write = Path.cwd()/"dbs_avg_cp.txt"
+    fp_write.touch()
+
+    cp_list= []
+
+    with fp_read.open(mode="r", encoding="UTF8", newline="") as file:
+        reader = csv.reader(file) # create csv reader object using csv
+        next(reader)              # to skip reading header
+        for row in reader:        # iterate each row with loop
+            # print(row)            # just to inspect, can remove later
+            cp_list.append(float(row[2]))
+
+        # print(cp_list)  # just to check, can remove later
+
+        # Calculate average closing price
+        avg_cp = sum(cp_list)/len(cp_list)
+
+    # write the result to a text file
+    with fp_write.open(mode="w", encoding="UTF8", newline="") as file:
+        file.write(f"average closing price for DBS = SGD{avg_cp}")
