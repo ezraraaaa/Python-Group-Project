df6ac7fdb304cac2d8c099f4b304a29b7e17effe
NEXJESsofea <s10255783@connect.np.edu.sg>
2023-07-23 22:34:20 +0800

    file text coh update

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -30,48 +30,49 @@ def coh_function():
 
         ## "for loop" is used to repeat the code according to the range given
         ## in this case 90.
-        for num in number_of_days:
+        with fp_write.open(mode="a", encoding="UTF8", newline="") as file:
+            for num in number_of_days:
 
-            ## a variable "diff" is assigned to a formula
-            ## to calculate the difference between the value of cash on hand
-            ## between the day after and the current day
-            diff = coh_list[num + 1][1] - coh_list[num][1]
+                ## a variable "diff" is assigned to a formula
+                ## to calculate the difference between the value of cash on hand
+                ## between the day after and the current day
+                diff = coh_list[num + 1][1] - coh_list[num][1]
 
-        ## Use of "if" condiiton
-        ## If the difference in cash is more 0
-        ## it is a cash surplus which would be
-        ## appended to the surplus list
-            if diff > 0:
-                surplus.append(diff)
-            
-            ## If the difference calculated  for the day is less than zero,
-            ## it is a cash deficit.
-            ## The day and the amount of deficit will then be printed in a statement 
-            ## for each day cash deficit occurs.
-            elif diff < 0:
-                # file.write(f"\n[CASH DEFICIT] DAY: {num + 1}, AMOUNT: USD{diff * -1}")
-                print(f"[CASH DEFICIT] DAY: {num + 1}, AMOUNT: USD{diff * -1}")
-            
-        ## Use of "if" condition
-        ## If the length of surplus list is equal
-        ## to the number of days(90), the statement would be printed
-            if len(surplus) == 90:  
-                # file.write(f"\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-                print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-        ## set of codes under the if condition will run if there are values in the surplus variable
-                if surplus:
-        ## max() is used to find the highest amount in the surplus list
-                    max_surplus = max(surplus)
-                    # use enumerate to include a counter for each loop
-                    for count, num in enumerate(surplus):
-                        # loop will stop once value of surplus is the highest
-                        if num == max_surplus:
-                            break
-    
-        ## Statement is printed to show the day with the highest cash surplus
-        ## as well as the amount of cash surplus
-                    file.write(f"\n[HIGHEST CASH SURPLUS] DAY: {count + 1}, AMOUNT: USD{max(surplus)}")
-                    print(f"[HIGHEST CASH SURPLUS] DAY: {count + 1}, AMOUNT: USD{max(surplus)}")
+            ## Use of "if" condiiton
+            ## If the difference in cash is more 0
+            ## it is a cash surplus which would be
+            ## appended to the surplus list
+                if diff > 0:
+                    surplus.append(diff)
+                
+                ## If the difference calculated  for the day is less than zero,
+                ## it is a cash deficit.
+                ## The day and the amount of deficit will then be printed in a statement 
+                ## for each day cash deficit occurs.
+                elif diff < 0:
+                    # file.write(f"\n[CASH DEFICIT] DAY: {num + 1}, AMOUNT: USD{diff * -1}")
+                    print(f"[CASH DEFICIT] DAY: {num + 1}, AMOUNT: USD{diff * -1}")
+                
+            ## Use of "if" condition
+            ## If the length of surplus list is equal
+            ## to the number of days(90), the statement would be printed
+                if len(surplus) == 90:  
+                    # file.write(f"\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+                    print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+            ## set of codes under the if condition will run if there are values in the surplus variable
+                    if surplus:
+            ## max() is used to find the highest amount in the surplus list
+                        max_surplus = max(surplus)
+                        # use enumerate to include a counter for each loop
+                        for count, num in enumerate(surplus):
+                            # loop will stop once value of surplus is the highest
+                            if num == max_surplus:
+                                break
+        
+            ## Statement is printed to show the day with the highest cash surplus
+            ## as well as the amount of cash surplus
+                        file.write(f"\n[HIGHEST CASH SURPLUS] DAY: {count + 1}, AMOUNT: USD{max(surplus)}")
+                        print(f"[HIGHEST CASH SURPLUS] DAY: {count + 1}, AMOUNT: USD{max(surplus)}")
                 
                     
 
