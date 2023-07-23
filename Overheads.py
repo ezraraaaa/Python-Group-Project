8181f5127ff7deea3509807f25aff4478841387d
NEXJESsofea <s10255783@connect.np.edu.sg>
2023-07-23 13:38:08 +0800

    overheaads and coh

--- Overheads.py
+++ Overheads.py
@@ -1,32 +1,27 @@
-## import required modules
 from pathlib import Path
 import csv
 
-# ## we can use this to inspect/check what is cwd:
-# print(Path.cwd())
+# create a file to csv file.
+fp =Path.cwd()/"CSV_Reports"/"Cash_on_Hand.csv"
 
-# ## setup filepaths for reading:
-fp_read = Path.cwd()/"CSV_Reports"/"Overheads.csv"
-
-def coh_function():
-    fp_read = Path.cwd()/"CSV_Reports"/"Overheads.csv"
-    fp_write = Path.cwd()/"dbs_avg_cp.txt"
-    fp_write.touch()
-
-    cp_list= []
-
-    with fp_read.open(mode="r", encoding="UTF8", newline="") as file:
-        reader = csv.reader(file) # create csv reader object using csv
-        next(reader)              # to skip reading header
-        for row in reader:        # iterate each row with loop
-            # print(row)            # just to inspect, can remove later
-            cp_list.append(float(row[2]))
-
-        print(cp_list)  # just to check, can remove later
-
-        # Calculate average closing price
-        avg_cp = sum(cp_list)/len(cp_list)
-
-    # write the result to a text file
-    with fp_write.open(mode="w", encoding="UTF8", newline="") as file:
-        file.write(f"average closing price for DBS = SGD{avg_cp}")
+def overheads_func():
+        
+    largest = 0
+    name = ''
+    # read the csv file to append profit and quantity from the csv.
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        
+        #skip the header row in the csv file 
+        next(reader) 
+    
+    #iterate each row in the csv file
+        for row in reader:
+            #check if the profit in the current row is larger than the current largest overhead
+            if float(row[1]) >= largest:
+                #update the largest overhead and its corresponding name
+                largest = float(row[1])
+                name = row[0]
+                
+    #print highest overhead and its corresponding name
+    print (f"[HIGHEST OVERHEAD] {name}: {largest}%")
--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,27 +1,74 @@
-## import required modules
 from pathlib import Path
 import csv
 
 # ## we can use this to inspect/check what is cwd:
 # print(Path.cwd())
 
-# ## setup filepaths for reading:
-fp_read = Path.cwd()/"CSV_Reports"/"Cash_on_Hand.csv"
+# ## setup filepaths for reading the Cash_on_Hand.csv:
+fp = Path.cwd()/"CSV_Reports"/"Cash_on_Hand.csv"
+fp_write = Path.cwd()/"summary_report.txt"
 
 def coh_function():
-    fp_read = Path.cwd()/"CSV_Reports"/"Cash_on_Hand.csv"
+    ## open the csv to read the data in it
+    with fp.open(mode="r",encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        ## skip the first row which is the header of the values
+        next(reader)
 
-    diff_list= []
+        # created an empty list to input data in it
+        coh_list = []
 
-    with fp_read.open(mode="r", encoding="UTF8", newline="") as file:
-        reader = csv.reader(file) # create csv reader object using csv
-        next(reader)              # to skip reading header
-        for row in reader:        # iterate each row with loop
-            # print(row)            # just to inspect, can remove later
-            diff_list.append(float(row[2]))
+        ## for loop is used append the data of the day and cash on hand values 
+        ## in each row into the empty list
+        for row in reader:
+            coh_list.append([int(row[0]),int(row[1])])
 
-        # print(cp_list)  # just to check, can remove later
+# opening the file to append
+    with fp_write.open(mode="a", encoding="UTF-8", newline="") as file:
+        ## a variable is assigned to a range from 0 to 90
+        ## to represent the total number of days of the csv
+        number_of_days = range(0,90)
+        ## empty list is created to accumulate and assign all the surplus value
+        surplus = []
 
-        # Calculate average closing price
-        # avg_cp = sum(cp_list)/len(cp_list)
+        ## "for loop" is used to repeat the code according to the range given
+        ## in this case 90.
+        for num in number_of_days:
 
+            ## a variable "diff" is assigned to a formula
+            ## to calculate the difference between the value of cash on hand
+            ## between the day after and the current day
+            diff = coh_list[num + 1][1] - coh_list[num][1]
+
+        ## Use of "if" condiiton
+        ## If the difference in cash is more 0
+        ## it is a cash surplus which would be
+        ## appended to the surplus list
+            if diff > 0:
+                surplus.append(diff)
+            
+            ## If the difference calculated  for the day is less than zero,
+            ## it is a cash deficit.
+            ## The day and the amount of deficit will then be printed in a statement 
+            ## for each day cash deficit occurs.
+            elif diff < 0:
+                file.write(f"\n[CASH DEFICIT] DAY: {num + 1}, AMOUNT: USD{diff * -1}")
+                print(f"[CASH DEFICIT] DAY: {num + 1}, AMOUNT: USD{diff * -1}")
+            
+        ## Use of "if" condition
+        ## If the length of surplus list is equal
+        ## to the number of days(90), the statement would be printed
+            if len(surplus) == 90:  
+                file.write(f"\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+                print(f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+        ## set of codes under the if condition will run if there are values in the surplus variable
+                if surplus:
+        ## max() is used to find the highest amount in the surplus list
+                    max_surplus = max(surplus)
+        ## To find the day in which the highest amount of surplus occurs 
+                    day_of_max_surplus = surplus.index(max_surplus) + 1
+        ## Statement is printed to show the day with the highest cash surplus
+        ## as well as the amount of cash surplus
+                    file.write(f"\n[HIGHEST CASH SURPLUS] DAY: {day_of_max_surplus}, AMOUNT: USD{max(surplus)}")
+                    print(f"[HIGHEST CASH SURPLUS] DAY: {day_of_max_surplus}, AMOUNT: USD{max(surplus)}")
+                    
